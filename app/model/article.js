472862c3d16f2f8d15b6a0905197683e8bfcdc06
wangmingg <wangmingg@chanjet.com>
2023-11-03 11:35:27 +0800

    更新egg

--- app/model/article.js
+++ app/model/article.js
@@ -7,9 +7,8 @@
 
 'use strict';
 
-
-module.exports = app => {
-  const { STRING, INTEGER, TEXT } = app.Sequelize;
+module.exports = (app) => {
+  const { STRING, INTEGER, TEXT, FLOAT, BLOB } = app.Sequelize;
   const Article = app.model.define('articles', {
     id: {
       primaryKey: true,
@@ -87,11 +86,98 @@ module.exports = app => {
       type: INTEGER,
       allowNull: false,
     },
+    text1: {
+      type: TEXT,
+      comment: '保留字段1',
+    },
+    text2: {
+      type: TEXT,
+      comment: '保留字段2',
+    },
+    text3: {
+      type: TEXT,
+      comment: '保留字段3',
+    },
+    text4: {
+      type: TEXT,
+      comment: '保留字段4',
+    },
+    text5: {
+      type: TEXT,
+      comment: '保留字段5',
+    },
+    text6: {
+      type: TEXT,
+      comment: '保留字段6',
+    },
+    text7: {
+      type: TEXT,
+      comment: '保留字段7',
+    },
+    text8: {
+      type: TEXT,
+      comment: '保留字段8',
+    },
+    text9: {
+      type: TEXT,
+      comment: '保留字段9',
+    },
+    int1: {
+      type: INTEGER,
+      comment: '整数1',
+    },
+    int2: {
+      type: INTEGER,
+      comment: '整数2',
+    },
+    int3: {
+      type: INTEGER,
+      comment: '整数3',
+    },
+    int4: {
+      type: INTEGER,
+      comment: '整数4',
+    },
+    int5: {
+      type: INTEGER,
+      comment: '整数5',
+    },
+    float1: {
+      type: FLOAT,
+      comment: '小数1',
+    },
+    float2: {
+      type: FLOAT,
+      comment: '小数2',
+    },
+    float3: {
+      type: FLOAT,
+      comment: '小数3',
+    },
+    float4: {
+      type: FLOAT,
+      comment: '小数4',
+    },
+    float5: {
+      type: FLOAT,
+      comment: '小数5',
+    },
+    blob: {
+      type: BLOB,
+      comment: '二进制数据',
+    },
+    extend_info: {
+      type: TEXT,
+      comment: '拓展字段',
+    },
   });
   Article.associate = () => {
     app.model.Article.hasMany(app.model.Comment, { as: 'comments' });
     app.model.Article.hasMany(app.model.Favorite);
-    app.model.Article.belongsTo(app.model.User, { as: 'user', foreignKey: 'uid' });
+    app.model.Article.belongsTo(app.model.User, {
+      as: 'user',
+      foreignKey: 'uid',
+    });
     app.model.Article.belongsTo(app.model.Category, { as: 'category' });
     app.model.Article.belongsTo(app.model.Tag, { as: 'tag' });
   };
